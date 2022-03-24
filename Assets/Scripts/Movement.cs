9cc2a29c7e893e4afa89e185734c8520218d35ee
DropxLr <dropxlr@gmail.com>
2022-03-24 18:43:02 +0000

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float mainThrust = 1000f;
+    public float rotateThrust = 100f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+ 
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotateThrust);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotateThrust);
+        }
+
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; //freezes rotation (physics) so you can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; //enables rotation again so that physics can apply rotation again
+    }
+}
