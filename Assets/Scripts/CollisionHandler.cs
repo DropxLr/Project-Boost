7219c7fa7fb82a2d865a75948bb8f51ae33ad446
DropxLr <dropxlr@gmail.com>
2022-03-26 14:01:17 +0000

    Added new scenes & updated CollisionHandler script to load next scene & reload first scene when finished

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,26 +1,55 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
+    public float loadDelay = 2f;
+
+    void OnCollisionEnter(Collision other) // behaviour when colliding with gameObjects 
     {
         switch (other.gameObject.tag)
         {
             case "Friendly":
-            Debug.Log("Friendly");
-            break;
+                Debug.Log("Friendly");
+                break;
 
             case "Fuel":
-            Debug.Log("Fuel");
-            break;
+                Debug.Log("Fuel");
+                break;
 
             case "Finish":
-            Debug.Log("Finish");
-            break;
+                LoadNextScene();
+                break;
 
-            default:
-            Debug.Log("Explode!!");
-            break;
+            default: // is the result of colliding with anything untagged
+                StartCrashSequence();
+                break;
         }
+
+        void StartCrashSequence()
+        {
+            // need to add SFX & particle effect on crash
+            GetComponent<MoveRocket>().enabled = false;
+            ReloadScene();
+        }
+
+        void ReloadScene() //reloads current scene when colliding with anything untagged in the switch statements above
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+
+        void LoadNextScene() //loads next scene in the build index (see build settings)
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+            if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            {
+                nextSceneIndex = 0;
+            }
+
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+
     }
 }
