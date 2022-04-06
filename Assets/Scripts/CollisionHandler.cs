e390c64fa6f4060a4be936c31d2dc7eda6ba1b5b
DropxLr <dropxlr@gmail.com>
2022-04-06 20:18:44 +0100

    Refactor and added debug controls to load next level and disable colliders

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class CollisionHandler : MonoBehaviour
 {
     public float loadDelay = 2f;
+    float debugLoadDelay = 0.5f;
     public AudioClip success;
     public AudioClip crashExplosion;
     public ParticleSystem explosionParticles;
@@ -11,15 +14,34 @@ public class CollisionHandler : MonoBehaviour
     AudioSource audioSource;
 
     bool isTransitioning = false;
+    bool collisionDisabled = false;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
+    void FixedUpdate()
+    {
+        activateDebugKeys();
+    }
+
+    void activateDebugKeys()
+    {
+        if (Keyboard.current.lKey.isPressed || Gamepad.current.buttonNorth.isPressed)
+        {
+            Util.DelayedCall(this, debugLoadDelay, () => { LoadNextScene(); });
+        }
+
+        if (Keyboard.current.cKey.isPressed || Gamepad.current.buttonWest.isPressed)
+        {
+            Util.DelayedCall(this, debugLoadDelay, () => { collisionDisabled = !collisionDisabled; });  //toggle collision
+        }
+    }
+
     void OnCollisionEnter(Collision other) // behaviour when colliding with gameObjects 
     {
-        if (isTransitioning) { return; } //stops triggering if sequence is already running
+        if (isTransitioning || collisionDisabled) { return; } //stops triggering if sequence is already running
 
         switch (other.gameObject.tag) //uses tags to trigger action
         {
@@ -35,53 +57,47 @@ public class CollisionHandler : MonoBehaviour
                 StartCrashSequence();
                 break;
         }
-
-        void StartSuccessSequence() // can trigger crash sound if it slips
-        {
-            isTransitioning = true;
-            audioSource.Stop();
-            audioSource.PlayOneShot(success);
-            // need to amend SFX above & add particle effect on success
-            GetComponent<MoveRocket>().enabled = false;
-            // using the util class to invoke a coroutine to delay the given function
-            Util.DelayedCall(this, loadDelay, () =>
-            {
-                LoadNextScene();
-            });
-        }
-
-        void StartCrashSequence()
-        {
-            isTransitioning = true;
-            audioSource.Stop();
-            audioSource.PlayOneShot(crashExplosion);
-            explosionParticles.Play();
-
-            GetComponent<MoveRocket>().enabled = false;
-            // using the util class to invoke a coroutine to delay the given function
-            Util.DelayedCall(this, loadDelay, () =>
-            {
-                ReloadScene();
-            });
-        }
-
-        void ReloadScene() //reloads current scene when colliding with anything untagged in the switch statements above
-        {
-            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentSceneIndex);
-        }
-
-        void LoadNextScene() //loads next scene in the build index (see build settings)
-        {
-            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            int nextSceneIndex = currentSceneIndex + 1;
-            if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
-            {
-                nextSceneIndex = 0;
-            }
-
-            SceneManager.LoadScene(nextSceneIndex);
-        }
-
     }
+
+    void StartSuccessSequence() // can trigger crash sound if it slips
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        // need to amend SFX above & add particle effect on success
+        GetComponent<MoveRocket>().enabled = false;
+        // using the util class to invoke a coroutine to delay the given function
+        Util.DelayedCall(this, loadDelay, () => { LoadNextScene(); });
+    }
+
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crashExplosion);
+        explosionParticles.Play();
+
+        GetComponent<MoveRocket>().enabled = false;
+        // using the util class to invoke a coroutine to delay the given function
+        Util.DelayedCall(this, loadDelay, () => { ReloadScene(); });
+    }
+
+    void ReloadScene() //reloads current scene when colliding with anything untagged in the switch statements above
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextScene() //loads next scene in the build index (see build settings)
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
 }
